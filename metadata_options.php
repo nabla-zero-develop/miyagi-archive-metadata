fef5d60622c386f19444b8ad818e6acb6780a098
nz-obata <obata@nabla-zero.jp>
2014-08-16 00:31:30 +0900

    公開保留をコメントアウトしたので、公開保留のときは非公開にする

--- metadata_options.php
+++ metadata_options.php
@@ -149,6 +149,7 @@ function output_kanko_status_selection($kanko_status, $caller){
 
 function output_open_level_selection($open_level, $caller){
 	$levels = array(array('-1','判断保留'),array('0','非公開'),array('1','公開'),array('2','限定公開')/*,array('3','公開保留')*/);
+	if($open_level == 3)$open_level = 0;
 	return selection('open_level', $levels, $open_level, $caller, 2).(is_numeric($open_level)?'':'基本情報整理表未入力');
 }
 
