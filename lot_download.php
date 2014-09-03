94fb1cba4d8393272403d5a7a672747131e83044
nz-obata <obata@nabla-zero.jp>
2014-09-03 14:40:44 +0900

    ロット情報ダウンロード

---
+++ lot_download.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'include/config.php';
+
+$lotid = intval($_REQUEST['lotid']);
+$sql = "select * from lotfile ".($lotid? "where lotid = $lotid": '');
+$res = mysql_query($sql);
+$rows = array(explode( ",","ユニークID,ロットID,順序,ファイルパス,終了"));
+while($row = mysql_fetch_row($res)){
+	$rows[] = $row;
+}
+
+mb_convert_variables('SJIS','UTF-8',$rows);
+
+header('Content-Type: application/octet-stream');
+header("Content-Disposition: attachment; filename=\"lot$lotid.csv\"");
+
+foreach($rows as $row){
+	foreach($row as &$col){
+		$col = '"'.str_replace('"', '""', $col).'"';
+	}
+	echo implode(',',$row)."\r\n";
+}
+
+?>
