bede7a91d852dbea5070f4d3d562f532d2d53198
nz-obata <obata@nabla-zero.jp>
2014-09-22 03:10:56 +0900

    ロット編集用CSVのダウンロードでDB変更を反映していなかった

--- lot_download.php
+++ lot_download.php
@@ -2,7 +2,7 @@
 require_once 'include/config.php';
 
 $lotid = intval($_REQUEST['lotid']);
-$sql = "select * from lotfile ".($lotid? "where lotid = $lotid": '');
+$sql = "select uniqid,lotid,ord,filepath,finish from lotfile ".($lotid? "where lotid = $lotid": '');
 $res = mysql_query($sql);
 $rows = array(explode( ",","ユニークID,ロットID,順序,ファイルパス,終了"));
 while($row = mysql_fetch_row($res)){
